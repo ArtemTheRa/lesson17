b359c2f9f2026b1b60943a3d98cdf85d3c6d4e08
ArtemTheRa <artyomthera@gmail.com>
2024-10-24 16:13:40 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,25 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+    print(a, c)
+    print()
+    print(a)
+#
+#
+# print_params()
+#
+#
+#
+# print_params(b=25)
+#
+#
+#
+# print_params(c=[1, 2, 3])
+#
+# values_list = [18, "18", False]
+# print_params(*values_list)
+#
+# values_dict = {'a': 20, 'b': 22, 'c': 24}
+# print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
